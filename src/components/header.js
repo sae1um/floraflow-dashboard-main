2d1001cc72b8f02cfc4a8747b1e0a93be7d67cb2
Godswill Erhunmwunse <113043388+sae1um@users.noreply.github.com>
2024-07-18 08:24:57 +0100

    Workinig Chart
    Chart now working with updated header style

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,14 @@
+import { HiOutlineSearch } from "react-icons/hi";
+
 export default function Header(){
     return(
-        <div className="text-center">
-            <input className="" type="text" placeholder="Search..."/>
+        <div className="bg-neutral-100 h-12">
+            <div className="flex flex-row items-center h-full pl-4">
+                <input className=" rounded-l-lg p-1" type="text" placeholder="Search..."/> 
+                <button className="bg-[#31CF23] p-2 rounded-r-lg hover:bg-green-400">
+                    <HiOutlineSearch />       
+                </button>
+            </div>
         </div>
     )
 }
