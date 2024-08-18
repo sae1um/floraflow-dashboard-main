0098737d7d02014cc7e6a262dca5c1eaeebf0b3a
Godswill Erhunmwunse <113043388+sae1um@users.noreply.github.com>
2024-08-18 14:25:13 +0000

    Updated connection mistakes

--- backend/database.js
+++ backend/database.js
@@ -9,11 +9,4 @@ const connection = mysql.createConnection({
     port: process.env.FF_DB_PORT
 })
 
-connection.connect((err) => {
-    if(err) {
-        console.log(err);
-    }
-    console.log("Connected " + connection.threadId);
-})
-
-// module.exports = { connection };
+module.exports = { connection };
