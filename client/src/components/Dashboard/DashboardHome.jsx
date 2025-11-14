c7a8ac2a03f5801780d371ee251f85792f39fc98
Godswill Erhunmwunse <mynameisoctowatcher@gmail.com>
2025-11-14 18:53:37 +0000

    Fixed Spoelling and import issues

--- client/src/components/Dashboard/DashboardHome.jsx
+++ client/src/components/Dashboard/DashboardHome.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router";
 
-export default function DashboardHomePage() {
+export default function DashboardHome() {
     return (
         <div>
             Main Dashboard
