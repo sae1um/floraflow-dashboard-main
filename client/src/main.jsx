cfaa172d3af27f32a9547384a7f553fd134f4e31
Godswill Erhunmwunse <mynameisoctowatcher@gmail.com>
2025-11-14 20:03:11 +0000

    Fixed resurfaced conflict

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,7 +13,7 @@ import { ProtectedRoute } from "./auth/ProtectedRoute";
 import OnboardingCheck from "./auth/OnboardingCheck";
 import OnboardingPage from "./pages/Onboarding";
 import DashboardHome from "./components/Dashboard/DashboardHome";
-import GreenhousesPage from "./components/Dashboard/GreenhosuesPage";
+import GreenhousesPage from "./components/Dashboard/GreenhousesPage";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
